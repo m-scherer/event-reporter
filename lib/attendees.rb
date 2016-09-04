3b150072bc3b570a6ac2f9bc459fd49ec5ad50d8
Michael Scherer <michael.c.scherer@gmail.com>
2016-09-04 14:01:05 -0600

    added attendees as an array of hashes

--- lib/attendees.rb
+++ lib/attendees.rb
@@ -1,12 +1,27 @@
 require "pry"
 require_relative "data_load"
+require_relative "data_scrub"
 
 class Attendees
   attr_reader :data
   include DataLoad
+  include DataScrub
 
   def initialize
     @data = DataLoad::load_file
+  end
+
+  def create_attendees
+    attendees = []
+    data.each do |row|
+      id = row[0]
+      first_name = row[:first_name]
+      last_name = row[:last_name]
+      zipcode = DataScrub::scrub_zipcode(row[:zipcode])
+      attendees << {:id=>id, :first_name=>first_name, :last_name=>last_name, :zipcode=>zipcode}
+    end
+    binding.pry
+    return attendees
   end
 
 end
--- test/attendees_test.rb
+++ test/attendees_test.rb
@@ -7,7 +7,7 @@ require "pry"
 class AttendeesTest < Minitest::Test
   include DataLoad
 
-  def test_if_attendees_is_created
+  def test_if_class_is_created
     assert_equal Attendees, Attendees.new.class
   end
 
@@ -15,6 +15,12 @@ class AttendeesTest < Minitest::Test
     attendees = Attendees.new
 
     refute nil, attendees.data
+  end
+
+  def test_if_attendees_exist
+    attendees = Attendees.new
+
+    assert_equal 5175, attendees.create_attendees.length
   end
 
 end
